09f978c6cb625a6ecbae240370320deef20d969e
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-08-29 20:33:34 +0700

    feat: lint fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import Hero from "./components/hero/Hero"
 /* import Navbar from "./components/layouts/Navbar" */
-import Footer from "./components/layouts/Footer"
+// import Footer from "./components/layouts/Footer"
 import About from "./components/about/About"
-import TechStack from "./components/tech-stack/TechStack"
+// import TechStack from "./components/tech-stack/TechStack"
 
 function App() {
   return (
